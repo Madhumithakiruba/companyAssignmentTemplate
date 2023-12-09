134df3da7d27e595cbe0890ca9fcecaaedc5213d
Madhumitha <madhukiruba99@gmail.com>
2023-12-09 07:46:34 +0000

    Company Assignment Template

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My Web App</h1>
+        </header>
+        <main>{}</main>
+      </div>
+    )
+  }
+}
+
+export default App
